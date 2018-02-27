5ce3b83dc549e5fbab4670232a28cc580dc69e8c
James Alzona <alzona.jam3s@gmail.com>
2018-02-27 21:25:17 +0800

    b4nc

--- js/init.js
+++ js/init.js
@@ -6,9 +6,30 @@ $(".button-collapse").sideNav();
 $(document).ready(function(){
 	$('.slider').slider({full_width: true});
 });
-      
+
+
+
+// Fade In Fade Out Menu Button
+$(document).ready(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.fixed-action-btn').fadeIn();
+		} else {
+			$('.fixed-action-btn').fadeOut();
+		}
+	});
+});
 
 
 
 
-        
+// SCROLL TO TOP BUTTON
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
+
