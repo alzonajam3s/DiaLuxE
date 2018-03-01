5cef7d736dc1c154dd303b13a01bfe78ea1089d7
unknown <alzona.jam3s@gmail.com>
2018-03-01 13:43:26 -0800

    b4nc

--- js/init.js
+++ js/init.js
@@ -40,3 +40,34 @@ function topFunction() {
     $('.modal').modal();
   });
           
+
+
+// FORM DROPDOWN
+
+  $(document).ready(function() {
+    $('select').material_select();
+  });
+
+
+
+
+ // CLEAR SUBMIT FORM
+ function submitForm() {
+   // Get the first form with the name
+   // Usually the form name is not repeated
+   // but duplicate names are possible in HTML
+   // Therefore to work around the issue, enforce the correct index
+   var frm = document.getElementsByName('contact-form')[0];
+   frm.submit(); // Submit the form
+   frm.reset();  // Reset all form data
+   return false; // Prevent page refresh
+}
+
+function clearform () {
+    document.getElementById("icon_telephone").value="";
+    document.getElementById("email").value="";
+    document.getElementById("subject").value="";
+    document.getElementById("message").value="";
+    return true;
+}
+            
