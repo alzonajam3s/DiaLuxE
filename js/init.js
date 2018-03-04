5fd51c5caa559a9eb71c436ca8283006cb551108
James Alzona <dubito@Jamess-MacBook-Pro.local>
2018-03-04 22:22:29 +0800

    b4nc

--- js/init.js
+++ js/init.js
@@ -55,3 +55,15 @@ function topFunction() {
       $('.parallax').parallax();
     });
         
+
+// PUSHPIN
+
+  $('.pushpin-demo-nav').each(function() {
+    var $this = $(this);
+    var $target = $('#' + $(this).attr('data-target'));
+    $this.pushpin({
+      top: $target.offset().top,
+      bottom: $target.offset().top + $target.outerHeight() - $this.height()
+    });
+  });
+        
